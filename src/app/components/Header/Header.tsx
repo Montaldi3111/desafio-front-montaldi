2ab502943ced20f158aed60d21e4ae75e02027fa
ignacio-montaldi <ignaciomontaldi@gmail.com>
2024-08-06 10:26:49 -0300

    Terminado home, arreglado el problema, añadido Storybook

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="bg-black p-4">
         <nav className="flex flex-row justify-between items-center">
-            <p className="text-ylw">DMH</p>
+            <p className="text-ylw text-xl">DMH</p>
             <span>
                 <button className="text-sm font-bold border-2 rounded py-2 px-4 border-ylw text-ylw mr-4">Ingresar</button>
                 <button className="text-sm font-bold bg-[#FAE208] rounded py-2 px-4 text-black">Crear Cuenta</button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
 import Image from "next/image";
+import Card from "./components/HomeCard/Card";
 export default function Home() {
+
+  const card1 = {
+    head: "Tranferí dinero",
+    content: "Desde Digital Money House vas a poder transferir dinero a otras cuentas, así como también recibir transferencias y nuclear tu capital en nuestra billetera virtual"
+  }
+
+  const card2 = {
+    head: "Pago de servicios",
+    content: "Pagá mensualmente los servicios en 3 simples clicks. Fácil, rápido y conveniente. Olvidate de las facturas en papel"
+  }
+
   return (
     <main className="relative">
-      <Image src="/home-img.png" alt="home-img" width={500} height={10} className="w-full"/>
+      <Image src="/home-img.png" alt="home-img" width={500} height={10} className="w-full" />
       <div className="absolute w-80 top-20 left-20">
         <h1 className="text-white text-4xl">De ahora en adelante, hacés más con tu dinero</h1>
         <h1 className="text-ylw text-2xl mt-3">Tu nueva <b>billetera</b> virtual</h1>
       </div>
-      <div className="w-100 bg-ylw py-20 relative rounded-t-5xl" id="content"></div>
+      <section className="w-full bg-ylw rounded-t-xl absolute bottom-0">
+      <div className="flex flex-row justify-around w-3/4 m-auto relative -top-20">
+        <Card message={card1}/>
+        <Card message={card2}/>
+      </div>
+      </section>
     </main>
   );
 }
