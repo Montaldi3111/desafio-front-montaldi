1eee47d0730f001b75539e9135b3ad344acfec89
ignacio-montaldi <ignaciomontaldi@gmail.com>
2024-10-14 22:57:50 -0300

    Solucionado error del middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,28 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
- 
+
 export async function middleware(request: NextRequest) {
   try {
     const token = request.cookies.get("token")?.value ?? "";
-    const session:string = request.cookies.get("session")?.value ?? "";
+    const session: string = request.cookies.get("session")?.value ?? "";
     const userRegistered = request.cookies.get("userRegistered")?.value;
 
-    if(!userRegistered && request.nextUrl.pathname === "/register/success") {
-      if(!token && session !== "true") {
-        return NextResponse.redirect(new URL("/", request.url));
+    if (!token && session !== "true") {
+      if (userRegistered) {
+        return NextResponse.next();
       } else {
-        return NextResponse.redirect(new URL('/login', request.url))
+        return NextResponse.redirect(new URL("/", request.url));
       }
+    } else { // existen token y session
+      return getAuthenticationHeaders(request, token, session);
+
     }
-    return getAuthenticationHeaders(request, token, session);
   } catch (error) {
     throw new Error("Could not get user account");
   }
 }
 
-const getAuthenticationHeaders = (request : NextRequest, accessToken : string, session : string) => {
+const getAuthenticationHeaders = (request: NextRequest, accessToken: string, session: string) => {
   const requestHeaders = new Headers(request.headers)
   requestHeaders.set('x-digital-access-token', accessToken)
   requestHeaders.set('x-digital-session', session)
